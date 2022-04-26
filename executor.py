e263f9428357e77aa401c87f2ea0bdfe7013604f
Alex C-G <alexcg@outlook.com>
2022-04-26 14:54:07 +0200

    feat: clean up doc.text first

--- executor.py
+++ executor.py
@@ -11,10 +11,27 @@ class SpacySentencizer(Executor):
     @requests(on="/index")
     def segment(self, docs: DocumentArray, **kwargs):
         min_length = 20  # minimum sentence length
-        sentencizer = Sentencizer()
+
+        # First do some cleanup of unwanted linebreaks
+        substitutions = [
+            # pre-emptively convert deliberate paragraph breaks into sentence-ends so they dont get caught by next entry in the list
+            {"old": "\n\n", "new": ". "},
+            {"old": "\r\r", "new": ". "},
+            # Remove incidental linebreaks caused by conversion
+            {"old": "\n", "new": " "},
+            {"old": "\r", "new": " "},
+        ]
+
+        for doc in docs:
+            if doc.text:
+                for sub in substitutions:
+                    doc.text = doc.text.replace(sub["old"], sub["new"])
+
+
+        # Sentencize
         nlp = English()
         nlp.add_pipe("sentencizer")
-
+        
         for doc in docs:
             sentences = DocumentArray()
             if doc.text:
